44cc40510e81fd982b7eed72fa50e9c30cd71feb
wira <wiradragneel5@gmail.com>
2025-05-06 01:27:52 +0700

    Initial commit with vulnerable application

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import subprocess
 
-
 def greet(name):
-    print(f"Hello, {name}!")
-
+	print(f"Hello, {name}!")
 
 def run_command(cmd):
-    subprocess.call(cmd, shell=True)
-
+	subprocess.call(cmd, shell=True)
 
 if __name__ == "__main__":
-    name = input("Enter your name: ")
-    greet(name)
+	name = input("Enter your name: ")
+	greet(name)
     
-    cmd = input("Enter a command to run: ")
-    run_command(cmd)
+	cmd = input("Enter a command to run: ")
+	run_command(cmd)
